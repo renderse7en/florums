5ace9309f7ca1caa4fd43e5987a5ee314316bd40
Jamin <jamin@se7enet.com>
2022-10-21 20:24:59 -0500

    ~ Updates to Forum migration

--- database/migrations/2022_10_22_003145_create_forums_table.php
+++ database/migrations/2022_10_22_003145_create_forums_table.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,20 +15,35 @@ return new class extends Migration
     public function up()
     {
         Schema::create('forums', function (Blueprint $table) {
+
+            // Columns
             $table->id();
             $table->string('name', 255);
             $table->text('description')->nullable();
             $table->text('rules')->nullable();
             $table->string('category', 255)->nullable();
-            $table->unsignedBigInteger('threads')->nullable();
-            $table->unsignedBigInteger('posts')->nullable();
-            $table->dateTime('private')->nullable();
+            $table->unsignedBigInteger('count_threads')->nullable();
+            $table->unsignedBigInteger('count_posts')->nullable();
+            $table->boolean('private')->nullable();
             $table->dateTime('locked')->nullable();
-            $table->foreignIdFor(User::class, 'created_by_id')->nullable();
-            $table->foreignIdFor(User::class, 'updated_by_id')->nullable();
-            $table->foreignIdFor(User::class, 'deleted_by_id')->nullable();
+            $table->foreignId('locked_by_id')->nullable();
+            $table->foreignId('created_by_id')->nullable();
+            $table->foreignId('updated_by_id')->nullable();
+            $table->foreignId('deleted_by_id')->nullable();
             $table->timestamps();
             $table->softDeletes();
+
+            // Indexes
+            $table->index('locked_by_id', 'IX_forums_locked_by_id');
+            $table->index('created_by_id', 'IX_forums_created_by_id');
+            $table->index('updated_by_id', 'IX_forums_updated_by_id');
+            $table->index('deleted_by_id', 'IX_forums_deleted_by_id');
+
+            // Foreign Keys
+            $table->foreign('locked_by_id', 'FK_forums_users_locked_by_id')->nullOnDelete();
+            $table->foreign('created_by_id', 'FK_forums_users_created_by_id')->nullOnDelete();
+            $table->foreign('updated_by_id', 'FK_forums_users_updated_by_id')->nullOnDelete();
+            $table->foreign('deleted_by_id', 'FK_forums_users_deleted_by_id')->nullOnDelete();
         });
     }
 
